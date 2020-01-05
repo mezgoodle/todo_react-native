282857907ccd594e18dc7d2947c48f0247ddd00e
mezgoodle <mezgoodle@gmail.com>
2020-01-05 22:14:04 +0200

    New component
    Create new component "Navbar" and some styles for himself

--- App.js
+++ App.js
@@ -1,19 +1,17 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Navbar } from "./src/Navbar";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Navbar title="UNOScore!" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    
+  }
 });
