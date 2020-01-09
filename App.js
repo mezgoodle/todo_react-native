851d9c08d25559d0d0cea90f4451325d47448089
mezgoodle <mezgoodle@gmail.com>
2020-01-09 21:30:28 +0200

    Add Todo component

--- App.js
+++ App.js
@@ -1,17 +1,41 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { Navbar } from "./src/Navbar";
+import { AddTodo } from './src/AddTodo';
+import { Todo } from './src/Todo';
 
 export default function App() {
+  const [todos, setTodos] = useState([])
+
+  const addtodo = (title) => {
+    const newTodo = {
+      id: Date.now().toString(),
+      title: title
+    }
+
+    setTodos(prev => [...prev, {
+      id: Date.now().toString(),
+      title: title
+    }])
+  }
+
   return (
-    <View style={styles.container}>
+    <View>
       <Navbar title="UNOScore!" />
+      <View style={styles.container}>
+        <AddTodo onSubmit={addtodo}/>
+
+        <View>
+          { todos.map(todo => <Todo todo={todo} key={todo.id} />) }
+        </View>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    
+    paddingHorizontal: 30,
+    paddingVertical: 20,
   }
 });
